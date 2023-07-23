2e9b53e66e6ec7b19ba42aa768605ddb118cc185
Philimuhire <philimuhire@gmail.com>
2023-07-23 07:16:44 +0200

    Created functions for basic operators functions

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Error: Cannot divide by zero!";
+  }
+  return a / b;
+}
